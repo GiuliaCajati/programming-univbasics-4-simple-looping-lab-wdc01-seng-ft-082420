34621654f0e7eb4792e7468f22d5c1bbbc3ebc6e
Giulia Cajati <giuliacajati@gmail.com>
2020-05-26 21:09:57 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -19,10 +19,16 @@ end
 def output_array(array)
   count = 0
   while count < array.length do
-    array[count].to_s 
+    puts array[count]
+    count +=1 
+  end 
 end 
 
 def return_string_array(array)
-  
+  count = 0
+  while count < array.length do
+    array[count].to_s 
+    count +=1 
+  end 
 end 
 
